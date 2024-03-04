572fb1931fc1db3cc67aaa70fc9c39f4a7300918
Mubarak68 <aloraifan505@hotmail.com>
2024-03-04 09:20:14 +0300

    Done with the task

---
+++ Codable.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+
+let jsonData = """
+{
+    "city": "Kuwait",
+    "temperature": 25,
+    "condition": "Sunny",
+    "forecast": [
+        {
+            "day": "Monday",
+            "high": 29,
+            "low": 17,
+            "condition": "Partly Cloudy"
+        },
+        {
+            "day": "Tuesday",
+            "high": 18,
+            "low": 13,
+            "condition": "Rain"
+        }
+    ]
+}
+""".data(using: .utf8)!
+
+struct Weather: Codable {
+    var city: String
+    var temperature: Double
+    var condition: String
+    var forecast: [Forecast]
+}
+
+struct Forecast: Codable{
+    var day: String
+    var high: Double
+    var low: Double
+    var condition: String
+}
+
+do {
+    let weather1 = try JSONDecoder().decode(Weather.self, from: jsonData)
+    print("The city is: \(weather1.city)")
+    
+    for weathers in weather1.forecast {
+        print("""
+----------------------------------------------
+The expected weather for this day: \(weathers.day)
+The expected high and low temp: \(weathers.high) , \(weathers.low)
+The expected condition of the day: \(weathers.condition)
+----------------------------------------------
+""")
+    }
+} catch {
+    print("Decoding error: \(error)")
+}
